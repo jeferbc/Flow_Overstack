8d527a440e1259c3d55b268a0ff795550522f445
jeferbc <jeffe.bernal@gmail.com>
2017-05-28 16:12:05 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  User.create(email: Faker::Internet.email, password: Faker::Internet.password(8))
+end
+10.times do
+  question = Question.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraphs, user_id: Faker::Number.between(1, 10))
+  Faker::Number.between(1, 10).times do |index|
+    question.comments.create(content: Faker::Lorem.paragraph, user_id: index + 1)
+    question.votes.create(liked: Faker::Boolean.boolean, user_id: index + 1)
+  end
+  Faker::Number.between(1, 10).times do
+      answer = question.answers.create(content: Faker::Lorem.paragraph, user_id: Faker::Number.between(1, 10))
+      Faker::Number.between(1, 10).times do |index|
+        answer.comments.create(content: Faker::Lorem.paragraph, user_id: index + 1)
+        answer.votes.create(liked: Faker::Boolean.boolean, user_id: index + 1)
+
+      end
+  end
+  Faker::Number.between(1, 10).times do |index|
+    question.comments.create(content: Faker::Lorem.paragraph, user_id: index + 1)
+    question.votes.create(liked: Faker::Boolean.boolean, user_id: index + 1)
+  end
+end
