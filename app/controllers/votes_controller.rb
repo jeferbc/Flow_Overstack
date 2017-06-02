b74549d1c41a44b97195fabf8726d68ce987fb36
jeferbc <jeffe.bernal@gmail.com>
2017-06-02 10:43:25 -0500

    votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,34 +1,20 @@
 class VotesController < ApplicationController
 
   def create
-    if (user_signed_in?)
-      unless (Vote.already_vote?(current_user.id, params[:vote][:votable_id], params[:vote][:votable_type]))
-        @vote = Vote.new(vote_params)
-        if @vote.save
-        else
-          flash[:alert] = "The Vote hasn't been saved, due an system error"
-        end
-      else
-        Vote.destroy_vote?(current_user.id, params[:vote][:votable_id], params[:vote][:votable_type])
-        flash[:notice] = "The Vote has been eliminated"
-      end
-    else
-      flash[:alert] = "You must been logged in order to vote"
-    end
-    respond_to do |f|
-      f.html {redirect_to question_path(id: params[:question_id])}
-    end
+    @vote = @votable.votes.new(vote_params)
+    @vote.user = current_user
+    @votes = @vote.calculate_votes(@votable)
+  end
+
+  def destroy
+    @vote = Vote.find(params[:id])
+    @votes = @vote.calculate_votes(@votable)
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def vote_comment
-      @vote = Vote.find(params[:id])
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def vote_params
-      params.require(:vote).permit(:amount, :votable_id, :votable_type, :user_id)
+      params.permit(:liked)
     end
 
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,7 +3,7 @@
 # Table name: votes
 #
 #  id           :integer          not null, primary key
-#  amount       :boolean
+#  liked        :boolean
 #  votable_type :string
 #  votable_id   :integer
 #  user_id      :integer
@@ -15,26 +15,31 @@ class Vote < ApplicationRecord
   belongs_to :votable, polymorphic: true
   belongs_to :user
 
-  def self.already_vote?(user_id, id, method)
-    if method == "Question"
-      votes = Question.find(id).votes
-    else
-      votes = Answer.find(id).votes
-    end
-    if votes.find_by(user_id: user_id) == nil
-      false
-    else
-      true
-    end
-  end
-  def self.destroy_vote?(user_id, id, method)
-    if method == "Question"
-      votes = Question.find(id).votes
-    else
-      votes = Answer.find(id).votes
-    end
-    votes.find_by(user_id: user_id).destroy 
+  scope :votes_up, -> { where(liked: true) }
+  scope :votes_down, -> { where(liked: false) }
+
+  def calculate_votes(votable)
+    votable.votes.votes_up.count - votable.votes.votes_down.count
   end
 
-
+  # def self.already_vote?(user_id, id, method)
+  #   if method == "Question"
+  #     votes = Question.find(id).votes
+  #   else
+  #     votes = Answer.find(id).votes
+  #   end
+  #   if votes.find_by(user_id: user_id) == nil
+  #     false
+  #   else
+  #     true
+  #   end
+  # end
+  # def self.destroy_vote?(user_id, id, method)
+  #   if method == "Question"
+  #     votes = Question.find(id).votes
+  #   else
+  #     votes = Answer.find(id).votes
+  #   end
+  #   votes.find_by(user_id: user_id).destroy
+  # end
 end
